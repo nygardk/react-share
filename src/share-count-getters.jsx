7100b85c31a5c0809e1520a34335f2099f07e74d
Vasilii <kuznecov_vasilii@.mail.ru>
2017-02-20 17:49:59 +0500

    added VK share count

--- src/share-count-getters.jsx
+++ src/share-count-getters.jsx
@@ -73,3 +73,25 @@ export function getPinterestShareCount(shareUrl, callback) {
     callback(!!data ? data.count : undefined);
   });
 }
+
+export function getVKShareCount(shareUrl, callback) {
+  if (!window.VK) {
+    window.VK = {
+      Share: {
+        count: (index, count) => window.VK.callbacks[index](count),
+      },
+      callbacks: [],
+    };
+  }
+
+  const url = 'https://vk.com/share.php';
+  const index = window.VK.callbacks.length;
+
+  window.VK.callbacks.push(callback);
+
+  return jsonp(url + objectToGetParams({
+    act: 'count',
+    index,
+    url: shareUrl,
+  }));
+}

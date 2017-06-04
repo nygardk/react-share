c5a1041fa8a2f21c596648c98d0b0df182f85841
Klaus Nygård <klaus.nygard@gmail.com>
2017-06-04 09:54:21 +0300

    expose shareLinks

--- src/react-share.jsx
+++ src/react-share.jsx
@@ -1,9 +1,11 @@
 import { generateIcon } from './generateIcon';
 import * as ShareButtons from './share-buttons';
 import * as ShareCounts from './share-counts';
+import * as shareLinks from './social-media-share-links';
 
 export {
   generateIcon as generateShareIcon,
   ShareButtons,
   ShareCounts,
+  shareLinks,
 };

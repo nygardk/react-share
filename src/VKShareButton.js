c7c80a20f2c1c427c918945d81af2c7200c56f58
Maxim Zasorin <maximzasorin@gmail.com>
2019-10-13 22:03:55 +0300

    update vk share button properties

--- src/VKShareButton.js
+++ src/VKShareButton.js
@@ -5,25 +5,28 @@ import assert from 'assert';
 import objectToGetParams from './utils/objectToGetParams';
 import createShareButton from './utils/createShareButton';
 
-function vkLink(url, { title, description, image }) {
+function vkLink(url, { title, image, noParse, noVkLinks }) {
   assert(url, 'vk.url');
 
   return 'https://vk.com/share.php' + objectToGetParams({
     url,
     title,
-    description,
     image,
+    noparse: noParse ? 1 : 0,
+    no_vk_links: noVkLinks ? 1 : 0,
   });
 }
 
 const VKShareButton = createShareButton('vk', vkLink, props => ({
   title: props.title,
-  description: props.description,
   image: props.image,
+  noParse: props.noParse,
+  noVkLinks: props.noVkLinks,
 }), {
   title: PropTypes.string,
-  description: PropTypes.string,
   image: PropTypes.string,
+  noParse: PropTypes.bool,
+  noVkLinks: PropTypes.bool,
 }, {
   windowWidth: 660,
   windowHeight: 460,

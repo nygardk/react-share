ecbf7001fec7806c8259a792bf3473062dbe4dcb
Klaus Nygård <klaus.nygard@gmail.com>
2019-12-27 17:45:47 +0200

    refactor iconFactory

--- src/EmailIcon.ts
+++ src/EmailIcon.ts
@@ -1,8 +1,9 @@
 import iconFactory from './utils/iconFactory';
 
-const EmailIcon = iconFactory('email', {
-  icon: 'M17,22v20h30V22H17z M41.1,25L32,32.1L22.9,25H41.1z M20,39V26.6l12,9.3l12-9.3V39H20z',
+const EmailIcon = iconFactory({
   color: '#7f7f7f',
+  networkName: 'email',
+  path: 'M17,22v20h30V22H17z M41.1,25L32,32.1L22.9,25H41.1z M20,39V26.6l12,9.3l12-9.3V39H20z',
 });
 
 export default EmailIcon;
--- src/utils/iconFactory.tsx
+++ src/utils/iconFactory.tsx
@@ -1,60 +1,50 @@
 import React from 'react';
-import PropTypes from 'prop-types';
 
-type Props = {
+type Props = Omit<React.SVGProps<SVGSVGElement>, 'width' | 'height'> & {
   borderRadius?: number;
-  className?: string;
   iconBgStyle?: React.CSSProperties;
   logoFillColor?: string;
   round?: boolean;
   size?: number;
 };
 
-export default function iconFactory(network: string, iconConfig: { color: string; icon: string }) {
-  const Icon: React.FC<Props> = props => {
-    const { className, iconBgStyle, logoFillColor, borderRadius, round, size } = props;
+type IconConfig = {
+  color: string;
+  networkName: string;
+  /** SVG path */
+  path: string;
+};
 
-    const baseStyle = {
-      width: size,
-      height: size,
-    };
+export default function iconFactory(iconConfig: IconConfig) {
+  const Icon: React.FC<Props> = ({
+    iconBgStyle,
+    logoFillColor,
+    borderRadius,
+    round,
+    size,
+    ...rest
+  }) => (
+    <svg viewBox="0 0 64 64" width={size} height={size} {...rest}>
+      <g>
+        {round ? (
+          <circle cx="32" cy="32" r="31" fill={iconConfig.color} style={iconBgStyle} />
+        ) : (
+          <rect
+            width="64"
+            height="64"
+            rx={borderRadius}
+            ry={borderRadius}
+            fill={iconConfig.color}
+            style={iconBgStyle}
+          />
+        )}
+      </g>
 
-    const classes = `social-icon social-icon--${network} ${className}`;
-
-    return (
-      <div style={baseStyle}>
-        <svg viewBox="0 0 64 64" width={size} height={size} className={classes}>
-          <g>
-            {!round ? (
-              <rect
-                width="64"
-                height="64"
-                rx={borderRadius}
-                ry={borderRadius}
-                fill={iconConfig.color}
-                style={iconBgStyle}
-              />
-            ) : (
-              <circle cx="32" cy="32" r="31" fill={iconConfig.color} style={iconBgStyle} />
-            )}
-          </g>
-
-          <g>
-            <path d={iconConfig.icon} fill={logoFillColor} />
-          </g>
-        </svg>
-      </div>
-    );
-  };
-
-  Icon.propTypes = {
-    borderRadius: PropTypes.number.isRequired,
-    className: PropTypes.string,
-    iconBgStyle: PropTypes.object.isRequired,
-    logoFillColor: PropTypes.string.isRequired,
-    round: PropTypes.bool,
-    size: PropTypes.number.isRequired,
-  };
+      <g>
+        <path d={iconConfig.path} fill={logoFillColor} />
+      </g>
+    </svg>
+  );
 
   Icon.defaultProps = {
     iconBgStyle: {},

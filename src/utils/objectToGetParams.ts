52088bc537763b4c750ee82a9ce2bf8cdc7d0b01
Klaus Nygård <klaus.nygard@gmail.com>
2019-12-28 11:08:52 +0200

    minor objectToGetParams fix

--- src/utils/objectToGetParams.ts
+++ src/utils/objectToGetParams.ts
@@ -1,11 +1,9 @@
 export default function objectToGetParams(object: {
   [key: string]: string | number | undefined | null;
 }) {
-  return (
-    '?' +
-    Object.entries(object)
-      .filter(([, value]) => value !== undefined && value !== null)
-      .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(value as string)}`)
-      .join('&')
-  );
+  const params = Object.entries(object)
+    .filter(([, value]) => value !== undefined && value !== null)
+    .map(([key, value]) => `${encodeURIComponent(key)}=${encodeURIComponent(String(value))}`);
+
+  return params.length > 0 ? `?${params.join('&')}` : '';
 }

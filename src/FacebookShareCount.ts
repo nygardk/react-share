a20190bb68cdb7da7994408fb9a58abb0b42ff93
Klaus Nygård <klaus.nygard@gmail.com>
2019-12-28 11:06:25 +0200

    fix FacebookShareCount

--- src/FacebookShareCount.ts
+++ src/FacebookShareCount.ts
@@ -3,11 +3,13 @@ import jsonp from 'jsonp';
 import createShareCount from './utils/createShareCount';
 
 function getFacebookShareCount(shareUrl: string, callback: (shareCount?: number) => void) {
-  const endpoint = `https://graph.facebook.com/?id=${shareUrl}`;
+  const endpoint = `https://graph.facebook.com/?id=${shareUrl}&fields=og_object{engagement}`;
 
   jsonp(endpoint, (err, data) => {
     callback(
-      !err && data && data.share && data.share.share_count ? data.share.share_count : undefined,
+      !err && data && data.og_object && data.og_object.engagement
+        ? data.og_object.engagement.count
+        : undefined,
     );
   });
 }

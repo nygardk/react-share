b5015500e075042e4b32c3df5ff7f649d05259aa
Nikita <warhammer-ss@yandex.ru>
2020-04-09 14:12:18 +0300

    Secure record in window.VK

--- src/VKShareCount.ts
+++ src/VKShareCount.ts
@@ -6,23 +6,20 @@ import createShareCount from './hocs/createShareCount';
 declare global {
   interface Window {
     VK: {
-      Share: {
+      Share?: {
         count: (index: number, count: number) => void;
       };
-      callbacks: ((count?: number) => void)[];
+      callbacks?: ((count?: number) => void)[];
     };
   }
 }
 
 function getVKShareCount(shareUrl: string, callback: (shareCount?: number) => void) {
-  if (!window.VK) {
-    window.VK = {
-      Share: {
-        count: (index, count) => window.VK.callbacks[index](count),
-      },
-      callbacks: [],
-    };
-  }
+  if (!window.VK) window.VK = {};
+  window.VK.Share = {
+    count: (index, count) => window.VK.callbacks![index](count),
+  };
+  window.VK.callbacks = [];
 
   const url = 'https://vk.com/share.php';
   const index = window.VK.callbacks.length;

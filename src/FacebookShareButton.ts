94f23d87314d077a47f2caeec255023292d16638
Lorenzo Bloedow <108244120+LorenzoBloedow@users.noreply.github.com>
2022-12-14 06:27:51 -0800

    Remove the 'quote' parameter (deprecated)

--- src/FacebookShareButton.ts
+++ src/FacebookShareButton.ts
@@ -2,24 +2,22 @@ import assert from './utils/assert';
 import objectToGetParams from './utils/objectToGetParams';
 import createShareButton from './hocs/createShareButton';
 
-function facebookLink(url: string, { quote, hashtag }: { quote?: string; hashtag?: string }) {
+function facebookLink(url: string, { hashtag }: { hashtag?: string }) {
   assert(url, 'facebook.url');
 
   return (
     'https://www.facebook.com/sharer/sharer.php' +
     objectToGetParams({
       u: url,
-      quote,
       hashtag,
     })
   );
 }
 
-const FacebookShareButton = createShareButton<{ quote?: string; hashtag?: string }>(
+const FacebookShareButton = createShareButton<{ hashtag?: string }>(
   'facebook',
   facebookLink,
   props => ({
-    quote: props.quote,
     hashtag: props.hashtag,
   }),
   {
